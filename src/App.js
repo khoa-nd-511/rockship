b25f9cc8b1916ffddf17e532d66689e9448c91da
EricKhoaMonger <n.d.khoa511@gmail.com>
2019-04-27 22:32:30 +0700

    [FEATURE] Show posts * Create Dashboard container * Create Post component * Adjust route

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Layout from './containers/Layout/Layout';
-
+import Dashboard from './containers/Dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/" component={null} />
+          <Route exact path="/" component={Dashboard} />
         </Switch>
       </Layout>
     </BrowserRouter>
---
+++ src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Post from '../../components/Post/Post';
+
+export class Dashboard extends Component {
+  render() {
+    return (
+      <Post />
+    )
+  }
+}
+
+export default Dashboard;
