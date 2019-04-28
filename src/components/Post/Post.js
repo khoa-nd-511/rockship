8017cbf2045c92f1f3bb2c00b9e976b9df7a71c4
EricKhoaMonger <n.d.khoa511@gmail.com>
2019-04-28 14:37:57 +0700

    [FEATURE] Show all posts * dynamically displaying Dashboard with react-streams * show all posts based on response

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,37 +1,31 @@
 import React from 'react';
-import { Button, Card, ListGroup, Container, Row, Col } from 'react-bootstrap';
+import { Button, Card, ListGroup, Col } from 'react-bootstrap';
 
 import './Post.css';
 
-const post = () => {
+const post = props => {
+  const { title, description } = props.post;
+  console.log(props.post)
   return (
-    <Container fluid>
-      <Row>
-        <Col xs={4}>
-          <Card className="post">
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-            </Card.Body>
+    <Col xs={4} className="mb-4">
+      <Card className="post">
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+        </Card.Body>
 
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>
-                <div className="chip">
-                  <img src="https://lh5.googleusercontent.com/-UZT5GSUGLd4/AAAAAAAAAAI/AAAAAAAAA-A/I_aPa22vfq4/photo.jpg" alt="asd" />
-                  <a href="!#">Khoa Nguyen</a>
-                </div>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button variant="primary" block>Read more</Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>
+            <div className="chip">
+              <img src="https://lh5.googleusercontent.com/-UZT5GSUGLd4/AAAAAAAAAAI/AAAAAAAAA-A/I_aPa22vfq4/photo.jpg" alt="asd" />
+              <a href="!#">Khoa Nguyen</a>
+            </div>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Button variant="primary" block>Read more</Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+    </Col>
   )
 }
 
--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react'
+import React, { Fragment } from 'react'
 import Post from '../../components/Post/Post';
+import { pipe } from 'rxjs';
+import { ajax } from "rxjs/ajax"
+import { map, startWith, switchMap } from "rxjs/operators"
+import { streamProps } from 'react-streams';
+import { Col } from 'react-bootstrap';
 
-export class Dashboard extends Component {
-  render() {
-    return (
-      <Post />
-    )
-  }
+const fetchPosts = pipe(
+  switchMap(_ => ajax('https://rockship-adbe4.firebaseio.com/posts.json').pipe(
+    map(data => data.response),
+    map(res => {
+      Object.keys(res)
+      if (Object.keys(res) === 0) {
+        return {
+          posts: [],
+          message: 'No posts at the moment'
+        }
+      }
+
+      return {
+        posts: res,
+        message: `Successfully fetched ${Object.keys(res).length} posts!!!`
+      }
+    })
+  )),
+  startWith({ posts: [], message: 'Fetching...' })
+)
+
+const Dashboard = streamProps(fetchPosts);
+
+export default () => {
+  return (
+    <Dashboard>
+      {
+        ({ posts, message }) => (
+          <Fragment>
+            <Col xs={12}><p>{message}</p></Col>
+            {Object.keys(posts).length > 0 && (
+              Object.keys(posts).map(k => (
+                <Post key={k} post={posts[k]} />
+              ))
+            )}
+          </Fragment>
+        )
+      }
+    </Dashboard >
+  )
 }
-
-export default Dashboard;
