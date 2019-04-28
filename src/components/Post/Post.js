e353275d9286156914f8beaf421141a532d8dbd2
EricKhoaMonger <n.d.khoa511@gmail.com>
2019-04-28 14:57:37 +0700

    [FEATURE] Show all posts * responsive dashboard component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -7,7 +7,7 @@ const post = props => {
   const { title, description } = props.post;
   console.log(props.post)
   return (
-    <Col xs={4} className="mb-4">
+    <Col xs={12} md={6} lg={4} className="mb-4">
       <Card className="post">
         <Card.Body>
           <Card.Title>{title}</Card.Title>
