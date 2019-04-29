66ee8a6f7314e21add9b318028de63c95b68f230
EricKhoaMonger <n.d.khoa511@gmail.com>
2019-04-29 22:39:21 +0700

    [AUTH] Guard routes * use context as a Provider in Layout

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,45 +1,51 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useReducer } from 'react';
 import { Container, Row } from 'react-bootstrap';
 
+import AuthContext from '../../context/authContext';
+import { reducer, initialState } from '../../context/authReducer';
 import Sidedrawer from '../../components/Sidedrawer/Sidedrawer';
 import Header from '../../components/Header/Header';
 import Backdrop from '../../components/Backdrop/Backdrop';
 
-export class Layout extends Component {
-  state = {
-    showSidedrawer: false
+const Layout = props => {
+  const [showSidedrawer, setShowSidedrawer] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { currentUser, isLoggedIn } = state;
+
+  const showSidedrawerHandler = () => {
+    setShowSidedrawer(true);
   }
 
-  showSidedrawerHandler = () => {
-    this.setState((prev, next) => {
-      return {
-        showSidedrawer: !prev.showSidedrawer
-      }
-    })
+  const closeSidedrawerHandler = () => {
+    setShowSidedrawer(false);
   }
 
-  closeSidedrawerHandler = () => {
-    this.setState({ showSidedrawer: false })
+  const logIn = (user) => {
+    dispatch({ type: 'LOG_IN', currentUser: user })
+  }
+  const logOut = () => {
+    dispatch({ type: 'LOG_OUT' })
   }
 
-  render() {
-    const { showSidedrawer } = this.state;
-
-    return (
-      <Fragment>
-        <Header toggleSidedrawer={this.showSidedrawerHandler} showSidedrawer={showSidedrawer} />
-        <div style={{ marginTop: 'calc(2rem + 72px)' }}>
-          <Container>
-            <Row>
-              {this.props.children}
-            </Row>
-          </Container>
-        </div>
-        <Sidedrawer showSidedrawer={showSidedrawer} closeSidedrawerHandler={this.closeSidedrawerHandler} />
-        <Backdrop showSidedrawer={showSidedrawer} clodeSidedrawer={this.closeSidedrawerHandler} />
-      </Fragment>
-    )
-  }
+  return (
+    <AuthContext.Provider value={{
+      currentUser,
+      isLoggedIn,
+      logIn,
+      logOut
+    }}>
+      <Header toggleSidedrawer={showSidedrawerHandler} showSidedrawer={showSidedrawer} />
+      <div style={{ marginTop: 'calc(2rem + 72px)' }}>
+        <Container>
+          <Row>
+            {props.children}
+          </Row>
+        </Container>
+      </div>
+      <Sidedrawer showSidedrawer={showSidedrawer} closeSidedrawerHandler={closeSidedrawerHandler} />
+      <Backdrop showSidedrawer={showSidedrawer} clodeSidedrawer={closeSidedrawerHandler} />
+    </AuthContext.Provider>
+  )
 }
 
 export default Layout
